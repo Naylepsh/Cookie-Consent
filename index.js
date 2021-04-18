176d0ad19f20c6eead4f36f36bc6152dda4fc34d
Naylepsh <naylepsh@gmail.com>
2021-04-18 12:19:59 +0200

    disable scroll

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
+let blur;
+
+/**
+ * BLUR
+ */
 const addBlur = () => {
-  const div = document.createElement("div");
-  div.classList.add("blur");
-  document.body.appendChild(div);
+  blur = document.createElement("div");
+  blur.classList.add("blur");
+  document.body.appendChild(blur);
+};
+
+const removeBlur = () => {
+  blur.remove();
+};
+
+/**
+ * SCROLLING
+ */
+const disableScrolling = () => {
+  const x = window.scrollX;
+  const y = window.scrollY;
+  window.onscroll = () => window.scrollTo(x, y);
+};
+
+const enableScrolling = () => {
+  window.onscroll = () => {};
+};
+
+/**
+ * COOKIE POPUP
+ */
+const addCookiePopup = () => {
+  disableScrolling();
+};
+
+const removeCookiePopup = () => {
+  enableScrolling();
 };
 
 window.onload = () => {
   addBlur();
+  addCookiePopup();
+  setTimeout(() => {
+    removeBlur(), enableScrolling();
+  }, 5000);
 };
