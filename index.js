4e4e4abfeab67346a8a325e084f26851c125263f
Naylepsh <naylepsh@gmail.com>
2021-04-19 13:37:25 +0200

    prettify buttons

--- index.js
+++ index.js
@@ -180,17 +180,22 @@ const createCheckboxSlider = () => {
 };
 
 const createCookiePopupFormButtons = ({ onReject }) => {
-  const accept = createButton({ text: "Accept" });
-  const reject = createButton({ text: "Reject", onClick: onReject });
+  const accept = createButton({ text: "Accept", classes: ["primary"] });
+  const reject = createButton({
+    text: "Reject",
+    onClick: onReject,
+    classes: ["danger"],
+  });
 
   return [accept, reject];
 };
 
-const createButton = ({ text, onClick, type }) => {
+const createButton = ({ text, onClick, type, classes }) => {
   const button = document.createElement("button");
   if (text) button.innerText = text;
   if (type) button.type = type;
   if (onClick) button.addEventListener("click", onClick);
+  if (classes) button.classList.add(...classes);
   return button;
 };
 
