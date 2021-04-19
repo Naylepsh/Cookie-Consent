54b0feb204a0837aa591423b7cbd4c2fbaad2efe
Naylepsh <naylepsh@gmail.com>
2021-04-19 16:53:38 +0200

    small cleanup

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const addCookiePopup = (vendors) => {
 const createCookiePopup = (vendors) => {
   const container = createCookiePopupContainer();
 
-  const title = createCookieTitle("GDPR Consent");
+  const title = createCookiePopupTitle("GDPR Consent");
   const form = createCookiePopupVendorForm(vendors);
   container.append(title, form);
 
@@ -56,7 +56,7 @@ const createCookiePopupContainer = () => {
   return container;
 };
 
-const createCookieTitle = (title) => {
+const createCookiePopupTitle = (title) => {
   const titleComponent = document.createElement("h2");
   titleComponent.classList.add("cookie-popup-title");
   titleComponent.innerText = title;
@@ -82,7 +82,7 @@ const createCookiePopupVendorForm = (vendors) => {
   form.addEventListener("submit", (event) => handleSubmit(event, inputName));
 
   const vendorList = createCookiePopupVendorList({ inputName, vendors });
-  const vendorsAccordion = createAccordion({
+  const vendorsAccordion = wrapInAccordion({
     title: "Vendor list",
     element: vendorList,
   });
@@ -96,24 +96,6 @@ const createCookiePopupVendorForm = (vendors) => {
   return form;
 };
 
-const createAccordion = ({ title, element }) => {
-  const toggler = createButton({
-    classes: ["secondary", "accordion-toggler"],
-    text: title,
-    onClick: (event) => {
-      element.classList.toggle("hidden");
-      event.target.classList.toggle("active");
-      event.preventDefault();
-    },
-  });
-  const accordion = document.createElement("div");
-  element.classList.add("hidden");
-
-  accordion.append(toggler, element);
-
-  return accordion;
-};
-
 const saveVendorsInCookie = (event, inputName) => {
   const inputs = event.target.elements[inputName];
   const acceptedVendorIds = Array.from(inputs)
@@ -125,12 +107,6 @@ const saveVendorsInCookie = (event, inputName) => {
     JSON.stringify(acceptedVendorIds),
     addHours(new Date(), 24)
   );
-};
-
-const addHours = (date, hours) => {
-  const copy = new Date(date);
-  copy.setTime(copy.getTime() + hours * 60 * 60 * 1000);
-  return copy;
 };
 
 const createCookiePopupVendorList = ({ inputName, vendors }) => {
@@ -199,12 +175,6 @@ const createCookiePopupVendorSlider = ({ inputName, vendorId, checked }) => {
   return label;
 };
 
-const createCheckboxSlider = () => {
-  const span = document.createElement("span");
-  span.classList.add("slider", "round");
-  return span;
-};
-
 const createCookiePopupFormButtons = ({ onReject }) => {
   const accept = createButton({
     text: "Accept",
@@ -221,39 +191,10 @@ const createCookiePopupFormButtons = ({ onReject }) => {
   return [accept, reject];
 };
 
-const createButton = ({ text, onClick, type, classes }) => {
-  const button = document.createElement("button");
-  if (text) button.innerText = text;
-  if (type) button.type = type;
-  if (onClick) button.addEventListener("click", onClick);
-  if (classes) button.classList.add(...classes);
-  return button;
-};
-
 const removeCookiePopup = () => {
   if (popup) {
     popup.remove();
   }
-};
-
-/**
- * MODAL
- */
-const wrapInModal = (element) => {
-  const modal = document.createElement("div");
-  modal.classList.add("modal");
-
-  const modalDialog = document.createElement("div");
-  modalDialog.classList.add("modal-dialog");
-
-  const modalBody = document.createElement("div");
-  modalBody.classList.add("modal-body");
-
-  modalBody.append(element);
-  modalDialog.append(modalBody);
-  modal.append(modalDialog);
-
-  return modal;
 };
 
 const setup = async () => {
@@ -269,6 +210,59 @@ const teardown = () => {
   removeCookiePopup();
   removeBlur();
   enableScrolling();
+};
+
+/**
+ * COMPONENTS
+ */
+const createCheckboxSlider = () => {
+  const span = document.createElement("span");
+  span.classList.add("slider", "round");
+  return span;
+};
+
+const createButton = ({ text, onClick, type, classes }) => {
+  const button = document.createElement("button");
+  if (text) button.innerText = text;
+  if (type) button.type = type;
+  if (onClick) button.addEventListener("click", onClick);
+  if (classes) button.classList.add(...classes);
+  return button;
+};
+
+const wrapInAccordion = ({ title, element }) => {
+  const toggler = createButton({
+    classes: ["secondary", "accordion-toggler"],
+    text: title,
+    onClick: (event) => {
+      element.classList.toggle("hidden");
+      event.target.classList.toggle("active");
+      event.preventDefault();
+    },
+  });
+  const accordion = document.createElement("div");
+  element.classList.add("hidden");
+
+  accordion.append(toggler, element);
+
+  return accordion;
+};
+
+const wrapInModal = (element) => {
+  const modal = document.createElement("div");
+  modal.classList.add("modal");
+
+  const modalDialog = document.createElement("div");
+  modalDialog.classList.add("modal-dialog");
+
+  const modalBody = document.createElement("div");
+  modalBody.classList.add("modal-body");
+
+  modalBody.append(element);
+  modalDialog.append(modalBody);
+  modal.append(modalDialog);
+
+  return modal;
 };
 
 /**
@@ -291,6 +285,15 @@ const fetchVendors = async () => {
   const url = "https://optad360.mgr.consensu.org/cmp/v2/vendor-list.json";
   const response = await fetch(url);
   return response.json();
+};
+
+/**
+ * Misc. utilities
+ */
+const addHours = (date, hours) => {
+  const copy = new Date(date);
+  copy.setTime(copy.getTime() + hours * 60 * 60 * 1000);
+  return copy;
 };
 
 window.onload = async () => {
